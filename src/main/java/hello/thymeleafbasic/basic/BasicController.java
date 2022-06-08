0e73713fec6291ec510861159be6c11bd7293143
YKS <styleyo12@naver.com>
2022-06-08 21:51:25 +0900

    thymeleaf - 3

--- src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -81,4 +81,25 @@ public class BasicController {
         model.addAttribute("localDateTime", LocalDateTime.now());
         return "basic/date";
     }
+    
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
+
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
+
 }
